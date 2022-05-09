945404854209d409cce476619587d95c204c0a69
Yuval <yuvaluner@gmail.com>
2022-05-09 18:26:35 +0300

    Added ratings and migrations for it

---
+++ AdvancedProgrammingProjectsServer/Migrations/20220509152238_ratings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdvancedProgrammingProjectsServer.Migrations
+{
+    public partial class ratings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AdvancedProgrammingProjectsServer/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedProgrammingProjectsServer.Models {
+
+    public class Rating {
+
+        public int Id { get; set; }
+
+        public int Stars { get; set; }
+
+        public string? Feedback { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime TimeSubmitted { get; set; }
+    }
+}
