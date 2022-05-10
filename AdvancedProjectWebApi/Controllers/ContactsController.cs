e7de7eb4b92ce902e0ea83010b8f663dceb1d536
Yuval <yuvaluner@gmail.com>
2022-05-10 22:04:31 +0300

    Added get/{id}/messages

--- AdvancedProjectWebApi/Controllers/ContactsController.cs
+++ AdvancedProjectWebApi/Controllers/ContactsController.cs
@@ -13,22 +13,19 @@ namespace AdvancedProjectWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ContactsController : ControllerBase
-    {
+    public class ContactsController : ControllerBase {
         private readonly AdvancedProgrammingProjectsServerContext _context;
 
-        public ContactsController()
-        {
+        public ContactsController() {
             _context = new AdvancedProgrammingProjectsServerContext();
         }
 
         // GET: api/Contacts
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<RegisteredUser>>> getContacts(string id)
-        {
+        public async Task<ActionResult<IEnumerable<RegisteredUser>>> getContacts(string id) {
             RegisteredUser user = await _context.RegisteredUser.Where(ru => ru.username == id).Include(ru => ru.contacts).FirstOrDefaultAsync();
             List<RegisteredUser> contacts = new List<RegisteredUser>();
-            foreach(Contact contact in user.contacts) {
+            foreach (Contact contact in user.contacts) {
                 contacts.Add(await _context.RegisteredUser.Where(ru => ru.username == contact.name).FirstOrDefaultAsync());
             }
             return contacts;
@@ -36,19 +33,15 @@ namespace AdvancedProjectWebApi.Controllers
 
         // GET: api/Contacts/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<RegisteredUser>> GetRegisteredUser(string id, string currentUser)
-        {
+        public async Task<ActionResult<RegisteredUser>> GetRegisteredUser(string id, string currentUser) {
             RegisteredUser registeredUser = await _context.RegisteredUser.Where(ru => ru.username == currentUser).Include(ru => ru.contacts).FirstOrDefaultAsync();
-            if (registeredUser == null)
-            {
+            if (registeredUser == null) {
                 return NotFound();
             }
-            if (registeredUser.contacts.Find(c => c.name == id) != null)
-            {
+            if (registeredUser.contacts.Find(c => c.name == id) != null) {
                 return await _context.RegisteredUser.Where(ru => ru.username != currentUser).FirstOrDefaultAsync();
             }
-            else
-            {
+            else {
                 return NotFound();
             }
         }
@@ -56,27 +49,21 @@ namespace AdvancedProjectWebApi.Controllers
         // PUT: api/Contacts/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutRegisteredUser(string id, RegisteredUser registeredUser)
-        {
-            if (id != registeredUser.username)
-            {
+        public async Task<IActionResult> PutRegisteredUser(string id, RegisteredUser registeredUser) {
+            if (id != registeredUser.username) {
                 return BadRequest();
             }
 
             _context.Entry(registeredUser).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RegisteredUserExists(id))
-                {
+            catch (DbUpdateConcurrencyException) {
+                if (!RegisteredUserExists(id)) {
                     return NotFound();
                 }
-                else
-                {
+                else {
                     throw;
                 }
             }
@@ -87,8 +74,7 @@ namespace AdvancedProjectWebApi.Controllers
         // POST: api/Contacts
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<RegisteredUser>> PostRegisteredUser(string user, string id)
-        {
+        public async Task<ActionResult<RegisteredUser>> PostRegisteredUser(string user, string id) {
             RegisteredUser userToAdd = await _context.RegisteredUser.Where(ru => ru.username == user).Include(ru => ru.contacts)
                 .Include(ru => ru.conversations).FirstOrDefaultAsync();
             if (userToAdd == null) {
@@ -101,18 +87,16 @@ namespace AdvancedProjectWebApi.Controllers
             userToAdd.contacts.Add(new Contact() { contactOf = user, name = id, LastSeen = DateTime.Now });
             userToAdd.conversations.Add(new Conversation() { with = user, messages = new List<Message>() });
             await _context.SaveChangesAsync();
-            
+
 
             return CreatedAtAction("GetRegisteredUser", new { id = userToAdd.username }, userToAdd);
         }
 
         // DELETE: api/Contacts/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteRegisteredUser(string id, string username)
-        {
+        public async Task<IActionResult> DeleteRegisteredUser(string id, string username) {
             RegisteredUser firstUser = await _context.RegisteredUser.Where(ru => ru.username == username).Include(ru => ru.contacts).Include(ru => ru.conversations).FirstOrDefaultAsync();
-            if (firstUser == null)
-            {
+            if (firstUser == null) {
                 return NotFound();
             }
             Contact contactFirst = firstUser.contacts.Find(c => c.name == id);
@@ -132,6 +116,19 @@ namespace AdvancedProjectWebApi.Controllers
             }
 
             return NoContent();
+        }
+
+        [HttpGet("{id}/messages")]
+        public async Task<ActionResult<IEnumerable<Message>>> getMessages(string id, string username) {
+
+            Contact con = await _context.Contact.Where(c => c.contactOf == username && c.name == id).FirstOrDefaultAsync();
+            if (con != null) {
+                Conversation convo = await _context.Conversation.Where(c => c.with == con.name).Include(c => c.messages).FirstOrDefaultAsync();
+                return convo.messages;
+            }
+            else {
+                return NotFound();
+            }
         }
 
         private bool RegisteredUserExists(string id)
