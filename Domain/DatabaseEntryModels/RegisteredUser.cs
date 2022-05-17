b5698a0e28127fc13c6014e7f9a8fd2621e3c819
Yuval <yuvaluner@gmail.com>
2022-05-17 17:53:20 +0300

    Added migration

--- Domain/DatabaseEntryModels/RegisteredUser.cs
+++ Domain/DatabaseEntryModels/RegisteredUser.cs
@@ -14,28 +14,28 @@ namespace Domain.DatabaseEntryModels {
 
         public string password { get; set; }
 
-        public string salt { get; set; }
+        public string? salt { get; set; }
 
-        public string hashingAlgorithm { get; set; }
+        public string? hashingAlgorithm { get; set; }
 
-        public string phone { get; set; }
+        public string? phone { get; set; }
 
-        public string email { get; set; }
+        public string? email { get; set; }
 
-        public string nickname { get; set; }
+        public string? nickname { get; set; }
 
-        public SecretQuestion secretQuestions { get; set; }
+        public SecretQuestion? secretQuestions { get; set; }
 
         public string? verificationcode { get; set; }
 
         public DateTime verificationCodeCreationTime { get; set; }
 
-        public List<Contact> contacts { get; set; }
+        public List<Contact>? contacts { get; set; }
 
-        public string nickNum { get; set; }
+        public string? nickNum { get; set; }
 
         public string? description { get; set; }
 
-        public List<Conversation> conversations { get; set; }
+        public List<Conversation>? conversations { get; set; }
     }
 }
