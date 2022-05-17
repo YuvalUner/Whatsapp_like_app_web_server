d53b30823a91c6979168dc48f08053f790b1acae
Yuval <yuvaluner@gmail.com>
2022-05-17 18:35:00 +0300

    Put a parameter in the right place

--- AdvancedProjectWebApi/Controllers/RegisteredUsersController.cs
+++ AdvancedProjectWebApi/Controllers/RegisteredUsersController.cs
@@ -141,7 +141,7 @@ namespace AdvancedProjectWebApi.Controllers
             return BadRequest();
         }
 
-        [HttpGet("/GetNickName")]
+        [HttpGet("{username}")]
         [Authorize]
         public async Task<IActionResult> getNickName(string? username)
         {
