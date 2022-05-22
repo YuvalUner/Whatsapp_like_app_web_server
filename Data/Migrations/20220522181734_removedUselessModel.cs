390cbf8c4305fa96de416c1e19c801e9a497dca0
Yuval <yuvaluner@gmail.com>
2022-05-22 21:19:18 +0300

    A very un-important migration

---
+++ Data/Migrations/20220522181734_removedUselessModel.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class removedUselessModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PendingUser_SecretQuestion_secretQuestionId",
+                table: "PendingUser");
+
+            migrationBuilder.DropTable(
+                name: "ActiveConnection");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "secretQuestionId",
+                table: "PendingUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nickname",
+                table: "PendingUser",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "PendingUser",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PendingUser_SecretQuestion_secretQuestionId",
+                table: "PendingUser",
+                column: "secretQuestionId",
+                principalTable: "SecretQuestion",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PendingUser_SecretQuestion_secretQuestionId",
+                table: "PendingUser");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "secretQuestionId",
+                table: "PendingUser",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "PendingUser",
+                keyColumn: "nickname",
+                keyValue: null,
+                column: "nickname",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nickname",
+                table: "PendingUser",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "PendingUser",
+                keyColumn: "email",
+                keyValue: null,
+                column: "email",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "PendingUser",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ActiveConnection",
+                columns: table => new
+                {
+                    ConnectionId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserAgent = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Username = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Identifier = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActiveConnection", x => new { x.ConnectionId, x.UserAgent, x.Username });
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PendingUser_SecretQuestion_secretQuestionId",
+                table: "PendingUser",
+                column: "secretQuestionId",
+                principalTable: "SecretQuestion",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
